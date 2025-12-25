a2093b1c6817ebfb35206fc889a9257a54280076
Paula Grangeiro <contato@paulagrangeiro.com.br>
2025-12-25 11:22:28 -0700

    Add list item

--- App.tsx
+++ App.tsx
@@ -1,15 +1,43 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [groceryItem, setGroceryItem] = useState("");
+  const [groceryList, setGroceryList] = useState<String[]>([]);
+
+  const handleAddItem = () => {
+    setGroceryList((currentGroceryList) => [
+      ...currentGroceryList,
+      groceryItem,
+    ]);
+  };
+
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.header}>Grocery List</Text>
       </View>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Insert here an item to shop"></TextInput>
-        <Button title="Add"></Button>
+        <TextInput
+          placeholder="Insert here an item to shop"
+          onChangeText={setGroceryItem}
+        ></TextInput>
+        <Button title="Add" onPress={handleAddItem}></Button>
       </View>
+      <FlatList
+        data={groceryList}
+        renderItem={(groceryItem) => (
+          <Text key={groceryItem.index}>{groceryItem.item}</Text>
+        )}
+      ></FlatList>
     </View>
   );
 }
